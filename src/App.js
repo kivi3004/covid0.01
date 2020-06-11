4d1264337058830592b2954ed57dcde54155acbb
kivi3004 <kirtyvijay999@gmail.com>
2020-06-11 21:53:11 +0530

    nCov_commit_v0.01 first commit of project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker} from './components';
+
+import styles from './App.module.css';
+import{ fetchData} from './api';
+import coronaImage from './images/image.png'
+class App extends React.Component{
+    state = {
+        data : {},
+        country: '',
+    }
+    async componentDidMount(){
+        const data = await fetchData();
+        this.setState({data: data});
+    }
+    handleCountryChange = async (country) => {
+        const data = await fetchData(country);
+    
+        this.setState({ data, country: country });
+      }
+    render(){
+        const { data, country } = this.state;
+
+        return(
+            <div className ={styles.container}>
+                <img classNames={styles.image} src={coronaImage} alt='COVID-19'/>
+                <Cards data = {data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+            </div>
+        )
+    }
+}
+
+export default App;
